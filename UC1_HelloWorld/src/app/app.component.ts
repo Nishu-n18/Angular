bd91adbc571ac5a30ba147afb6f9261d5a18e98e
Nishu-n18 <yrnishu03@gmail.com>
2025-03-05 16:04:14 +0530

    [nishu] added HelloWorld

---
+++ UC1_HelloWorld/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'UC1_HelloWorld';
+  ngOnInit():void{
+    this.title="Hello from BridgeLabz."
+  }
+}
